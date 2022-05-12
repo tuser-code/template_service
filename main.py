c2954171ab75519ee7cdc9567738348a71c08e4e
tuser-code <69112275+tuser-code@users.noreply.github.com>
2022-05-12 18:48:18 +0300

    asynchronous running is implemented

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import asyncio
+
+
+async def main():
+	await asyncio.sleep(2)
+	print('----')
+
+if __name__ == '__main__':
+	try:
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(main())
+		loop.close()
+	except KeyboardInterrupt:
+		logger.info('Exiting from App')
