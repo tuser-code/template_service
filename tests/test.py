346d589653e25f00ad63a84167eddb1d8ce5ef83
tuser-code <testtorrent2000@gmail.com>
2022-05-14 10:38:01 +0300

    test.py updated

--- tests/test.py
+++ tests/test.py
@@ -43,12 +43,20 @@ class TestGRPCClient(unittest.IsolatedAsyncioTestCase):
     self.grpc_client = GRPCClient('localhost:50055')
 
   async def test_get_doc_by_rpc_client_returned_type(self):
-    res = await self.grpc_client.get_doc('template_1.kv', val1=57, val2='sting value')
+    res = await self.grpc_client.get_doc('template_1.kv', val1=57, 
+      val2='sting value')
     self.assertIs(type(res), str)
 
   async def test_get_doc_by_rpc_client_is_error(self):
-    res = await self.grpc_client.get_doc('template_1.kv', val1=57, val2='text info', some_dict={'qw':25, 'as':12})
+    res = await self.grpc_client.get_doc('template_1.kv', 
+      quality_staff='Enes Çavdar', val1=57, val2='text info', 
+      data=[
+      {'proc_id':'1', 'proc_name':'tres', 'control_methods':'control_method'},
+      {'proc_id':'2', 'proc_name':'tres', 'control_methods':'control_method'},
+      {'proc_id':'3', 'proc_name':'tres', 'control_methods':'control_method'},
+      ])
     self.assertFalse('Error' in res)
+    print(res)
 
 
 if __name__ == '__main__':
